e362c4ec69284bec0118aef3a74e4fe43228beb8
Patin Muangjan <patin@patin.dev>
2026-08-11 23:06:27 +0700

    set user and email for git
    Cause that would be great idea!

--- modules/applications/git.nix
+++ modules/applications/git.nix
@@ -2,6 +2,10 @@
   den.aspects.git = {
     homeManager = {
       programs.git.enable = true;
+      programs.git.settings = {
+	      user.name = "Patin Muangjan";
+	      user.email = "patin@patin.dev";
+      };
     };
   };
 }
