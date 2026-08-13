f75449be8ab6419d26690ac105bbf46ece20c05c
Patin Muangjan <patin@patin.dev>
2026-08-13 15:29:23 +0700

    enable zram
    cause that would be a great idea

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -5,6 +5,11 @@
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
       boot.kernelPackages = pkgs.linuxPackages_latest;
+      zramSwap = {
+        enable = true;
+        memoryPercent = 50;
+        algorithm = "zstd";
+      };
     };
   };
 }
