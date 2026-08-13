7bd0ed4e202248656670e9f5629434292f90e319
Patin Muangjan <patin@patin.dev>
2026-08-13 11:43:48 +0700

    add mods to zen browser
    Add mods "Transparent Zen" to zen browser, Can't still figure out how to
    add Sine mods from the "Store"?

--- modules/applications/browsers.nix
+++ modules/applications/browsers.nix
@@ -28,6 +28,9 @@
         };
         profiles."279p" = {
           sine.enable = true;
+          sine.mods = [
+            "642854b5-88b4-4c40-b256-e035532109df"
+          ];
         };
       };
       home.sessionVariables = {
