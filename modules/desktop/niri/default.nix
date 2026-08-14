c423dc9eb255f4702b93d250d9f4410c4eb32bbd
Patin Muangjan <patin@patin.dev>
2026-08-14 12:09:27 +0700

    picture in picture good

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -318,10 +318,7 @@
             }
             {
               matches = [
-                {
-                  "app-id" = "firefox$";
-                  title    = "^Picture-in-Picture$";
-                }
+                { "app-id" = "firefox$"; title = "^Picture-in-Picture$"; }
               ];
               "open-floating" = true;
             }
