2eab588b82e727a7cc886f42de8bce19b4c63d48
Patin Muangjan <patin@patin.dev>
2026-08-14 07:22:59 +0700

    preserve bluetooth settings
    cause again that would be a good idea

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,6 +1,12 @@
 {
   den.aspects.bluetooth = {
     nixos = { lib, pkgs, ... }: {
+      preservation.preserveAt."/persistent" = {
+        directories = [
+          "/var/lib/bluetooth"
+        ];
+      };
+
       hardware.bluetooth.enable = true;
     };
   };
