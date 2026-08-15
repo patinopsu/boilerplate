d78d00217d6926dcaa6a00548167f1a0a279a1fe
Patin Muangjan <patin@patin.dev>
2026-08-15 22:55:26 +0700

    add nixos-hardware repos
    this repo is for the laptop hardware "quirks"

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
