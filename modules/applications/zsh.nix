f84253c928d05c647e2d02c2fc8a4347f2845b3f
Patin Muangjan <patin@patin.dev>
2026-08-16 19:57:41 +0700

    enable fastfetch
    the essentials

--- modules/applications/zsh.nix
+++ modules/applications/zsh.nix
@@ -19,6 +19,7 @@
 
       programs.cava.enable = true;
       programs.btop.enable = true;
+      programs.fastfetch.enable = true;
       programs.zsh = {
         enable = true;
         enableCompletion = true;
