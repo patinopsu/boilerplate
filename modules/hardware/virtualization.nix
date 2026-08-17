65ae1115941875e5a7e0710fc8d44bee33a946a3
Patin Muangjan <patin@patin.dev>
2026-08-17 21:35:00 +0700

    preserve libvirt directories

--- modules/hardware/virtualization.nix
+++ modules/hardware/virtualization.nix
@@ -1,8 +1,15 @@
 {
   den.aspects.virtualization = {
     nixos = { lib, pkgs, ... }: {
+      preservation.preserveAt."/persistent".directories = [
+        "/var/lib/libvirt"
+      ];
+
       virtualisation.libvirtd.enable = true;
       programs.virt-manager.enable = true;
+      environment.systemPackages = [
+        pkgs.swtpm
+      ];
     };
   };
 }
