dd4fc24b23a51c0f5de659fe4b3eb0f81b423c24
Patin Muangjan <patin@patin.dev>
2026-08-18 18:54:43 +0700

    enable nvidia container toolkit

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -2,6 +2,7 @@
   den.aspects.nvidia = {
     nixos = {
       services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia-container-toolkit.enable = true;
       hardware.nvidia = {
         open = true;
         branch = "bleeding_edge";
