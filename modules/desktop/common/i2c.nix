b188ef0eaa6cea3a541491dae3df0a97a7feb3f4
Patin Muangjan <patin@patin.dev>
2026-08-19 09:48:03 +0700

    ddcutils
    for i2c with noctalia or smth

--- modules/desktop/common/i2c.nix
+++ modules/desktop/common/i2c.nix
@@ -1,8 +1,12 @@
 { den, ... }: {
   den.aspects.i2c = { user, ... }: {
-    nixos = {
+    nixos = { pkgs, ... }: {
       hardware.i2c.enable = true;
       users.users.${user.name}.extraGroups = [ "i2c" ];
+
+      environment.systemPackages = [
+        pkgs.ddcutil
+      ];
     };
   };
 }
