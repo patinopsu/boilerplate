2b79b971ee2aeb4aa0ae7bcb53f2312c172e3fae
Patin Muangjan <patin@patin.dev>
2026-08-20 09:42:41 +0700

    finally getting the virtualization stuff working!
    I encountered the problem where the
    "virt-secret-init-encryption.service" would failed to initialize the
    secrets for swtpm, to fix this, I run this command "systemd-creds
    encrypt --with-key=tpm2 --name=secrets-encryption-key -
    /var/lib/libvirt/secrets/secrets-encryption-key" (thanks duck.ai)

--- modules/hardware/virtualization.nix
+++ modules/hardware/virtualization.nix
@@ -1,19 +1,20 @@
 {
-  den.aspects.virtualization = {
+  den.aspects.virtualization = { user, ... }: {
     nixos = { lib, pkgs, ... }: {
       preservation.preserveAt."/persistent".directories = [
         "/var/lib/libvirt"
       ];
 
-      virtualisation.libvirtd = {
-        enable = true;
-        qemu.swtpm.enable = true;
-      };
-
+      virtualisation.libvirtd.enable = true;
+      virtualisation.libvirtd.qemu.swtpm.enable = true;
+      networking.firewall.trustedInterfaces = [ "virbr0" ];
       programs.virt-manager.enable = true;
-      environment.systemPackages = [
-        pkgs.swtpm
+
+      environment.systemPackages = with pkgs; [ 
+        dnsmasq 
       ];
+
+      users.users.${user.name}.extraGroups = [ "libvirtd" "kvm" ];
     };
   };
 }
