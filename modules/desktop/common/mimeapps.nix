97a170469054b1e4b21711fe0d325ee489539b6a
Patin Muangjan <patin@patin.dev>
2026-08-23 16:50:19 +0700

    only purpose of this is to enable mimeapps
    maybe I'll just put it in any program that have xdg app associations

--- modules/desktop/common/mimeapps.nix
+++ modules/desktop/common/mimeapps.nix
@@ -1,40 +1,7 @@
-{ den, ... }: {
+{
   den.aspects.mimeapps = {
-    nixos = {
-      xdg.mimeApps = {
-        enable = true;
-
-        defaultApplications = {
-          # Web Browser
-          "x-scheme-handler/http" = [ "zen-twilight.desktop" ];
-          "x-scheme-handler/https" = [ "zen-twilight.desktop" ];
-          "x-scheme-handler/chrome" = [ "zen-twilight.desktop" ];
-          "text/html" = [ "zen-twilight.desktop" ];
-          "application/x-extension-htm" = [ "zen-twilight.desktop" ];
-          "application/x-extension-html" = [ "zen-twilight.desktop" ];
-          "application/x-extension-shtml" = [ "zen-twilight.desktop" ];
-          "application/xhtml+xml" = [ "zen-twilight.desktop" ];
-          "application/x-extension-xhtml" = [ "zen-twilight.desktop" ];
-          "application/x-extension-xht" = [ "zen-twilight.desktop" ];
-
-          # Archives
-          "application/zip:*.zip" = [ "org.kde.ark.desktop" ];
-          "application/zip:*.zipx" = [ "org.kde.ark.desktop" ];
-          "application/vnd.rar" = [ "org.kde.ark.desktop" ];
-
-          # Documents
-          "application/pdf" = [ "org.kde.okular.desktop" ];
-
-          # Image Viewer
-          "image/*" = [ "org.gnome.Loupe.desktop" ];
-          "image/png" = [ "org.gnome.Loupe.desktop" ];
-          "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
-          "image/jpg" = [ "org.gnome.Loupe.desktop" ];
-          "image/webp" = [ "org.gnome.Loupe.desktop" ];
-          "image/gif" = [ "org.gnome.Loupe.desktop" ];
-          "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
-        };
-      };
+    homeManager = {
+      xdg.mimeApps.enable = true;
     };
   };
 }
