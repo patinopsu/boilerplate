4e13e0244759bc4be5366aa7bab48a000e6c20d4
Patin Muangjan <patin@patin.dev>
2026-08-24 13:22:54 +0700

    initial support for secrets

---
+++ modules/security/secrets.nix
@@ -0,0 +1,20 @@
+{ inputs, ... }: {
+  den.aspects.secrets = { host, ... }: {
+    nixos = { pkgs, ...}: {
+      imports = [
+        inputs.sops-nix.nixosModules.sops
+      ];
+
+      sops = {
+        defaultSopsFile = ./secrets.yaml;
+        age.keyFile = "${host.settings.persistPath}/secrets/age-key.txt";
+        age.generateKey = false;
+      };
+
+      environment.systemPackages = [
+        pkgs.age
+        pkgs.sops
+      ];
+    };
+  };
+}
