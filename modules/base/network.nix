345896ceb3f9c66adca71edd9aa6413d5ae52fb5
Patin Muangjan <patin@patin.dev>
2026-08-24 13:24:47 +0700

    use dnsproxy instead of whatever dns that networkmanager use by default
    and enable also enable captive portal detection, will move this to
    different aspects altogether since not all machines need a captive
    protal

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -1,6 +1,6 @@
-{
-  den.aspects.network = {
-    nixos = {
+{ self, ... }: {
+  den.aspects.network = { host, ... }: {
+    nixos = { config, ... }: {
       preservation.preserveAt."/persistent"= {
         directories = [
           "/var/lib/NetworkManager"
@@ -8,9 +8,64 @@
         ];
       };
 
-      networking.networkmanager.enable = true;
-      networking.firewall.enable = true;
-      networking.nftables.enable = true;
+      programs.captive-browser.enable = true;
+
+      networking = {
+        networkmanager = {
+          enable = true;
+          dns = "none";
+          appendNameservers = [
+            "127.0.0.53"
+          ];
+
+          settings."connectivity" = {
+            enabled = true;
+            uri = "http://nmcheck.gnome.org/check_network_status.txt";
+            response = "OK";
+          };
+        };
+
+        firewall.enable = true;
+        nftables.enable = true;
+      };
+
+      sops = {
+        secrets."nextdns-id" = {
+          sopsFile = "${self.outPath}/secrets/nextdns-id.yaml";
+          key = "id";
+        };
+
+        templates."dnsproxy.yaml" = {
+          group = "keys";
+          mode = "0440";
+          path = "/run/dnsproxy.yaml";
+          content = ''
+            listen-addrs:
+              - 127.0.0.53
+            listen-ports:
+              - 53
+            upstream:
+              - https://dns.nextdns.io/${config.sops.placeholder.nextdns-id}/${host.settings.prettyName}
+            bootstrap:
+              - 1.1.1.1
+              - 9.9.9.9
+          '';
+        };
+      };
+
+      services.dnsproxy = {
+          enable = true;
+          flags = [ "--config-path=${config.sops.templates."dnsproxy.yaml".path}" ];
+      };
+      services.resolved.enable = false;
+
+      systemd.services.dnsproxy.serviceConfig = {
+        SupplementaryGroups = [ "keys" ];
+        BindReadOnlyPaths = [
+          "/run/secrets/rendered"
+          "${config.sops.templates."dnsproxy.yaml".path}"
+        ];
+      };
     };
   };
 }
