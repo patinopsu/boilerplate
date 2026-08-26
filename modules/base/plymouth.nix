ca6322fb3011cf5d14d9dddb3b583a114db1e8a6
Patin Muangjan <patin@patin.dev>
2026-08-26 14:58:40 +0700

    plymouth!
    yay, boot animation type stuff

--- modules/base/plymouth.nix
+++ modules/base/plymouth.nix
@@ -1,19 +1,22 @@
 {
   den.aspects.plymouth = {
-    boot = {
-      plymouth = {
-        enable = true;
-      };
+    nixos = {
+      boot = {
+        plymouth = {
+          enable = true;
+          theme = "bgrt";
+        };
 
-      # Enable "Silent boot"
-      consoleLogLevel = 3;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "rd.udev.log_level=3"
-        "rd.systemd.show_status=auto"
-      ];
+        # Enable "Silent boot"
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        kernelParams = [
+          "quiet"
+          "rd.udev.log_level=3"
+          "rd.systemd.show_status=auto"
+        ];
+      };
+      stylix.targets.plymouth.enable = false;
     };
-    stylix.targets.plymouth.enable = false;
   };
 }
