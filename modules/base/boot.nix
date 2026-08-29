507b911c516c5de315e10387bda3748641c090d6
Patin Muangjan <patin@patin.dev>
2026-08-29 21:53:45 +0700

    make kernel package overwriteable

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -1,11 +1,11 @@
 {
   den.aspects.boot = {
-    nixos = { pkgs, ... }: {
+    nixos = { pkgs, lib, ... }: {
       boot.loader.timeout = 0;
       boot.initrd.systemd.enable = true;
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
       zramSwap = {
         enable = true;
         memoryPercent = 50;
